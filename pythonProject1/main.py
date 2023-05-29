2e0b9bea41ca2e38570f09d028b001e73efa9487
Mallikarjun Kullolli <132191646+Mallikarjun210803@users.noreply.github.com>
2023-05-29 16:41:14 +0530

    Update main.py
    Code optimised

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -1,22 +1,16 @@
 import pyttsx3
 
-def speak(text):
-    engine.say(text)
-    engine.runAndWait()
-
 engine = pyttsx3.init()
 
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 engine.setProperty('rate', 160)
-engine.setProperty('volume',1)
+engine.setProperty('volume', 1)
+
 print("Hello, I am the RoboSpeaker! Created by Supreeth BH")
-while True:
+
+while text != "exit":
     text = input("Enter what you want me to speak:")
-    speak(text)
-    if text == "exit":
-        speak("goodbye friend it was pleasure talking to you")
-        break
+    engine.say(text, wait=False)
 
-
-
+speak("goodbye friend it was pleasure talking to you")
